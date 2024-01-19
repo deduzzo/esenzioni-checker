bed4009a903a4cb90422cf5106f0d698198592fc
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2024-01-19 14:26:32 +0100

    aggiornamento db

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -22,10 +22,10 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::class,
-                'only' => ['logout'],
+                'only' => ['logout','index'],
                 'rules' => [
                     [
-                        'actions' => ['logout'],
+                        'actions' => ['logout','index'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -73,6 +73,35 @@ class SiteController extends Controller
         return $this->render('index', [
             'model' => $model,
         ]);
+    }
+
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
     }
 
     /**
---
+++ models/User.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace app\models;
+
+class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+{
+    public $id;
+    public $username;
+    public $password;
+    public $authKey;
+    public $accessToken;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+            return new static([
+                "id" => $id,
+                "username" => $id,
+                "password" => $id,
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+ /*       foreach (self::$users as $user) {
+            if ($user['accessToken'] === $token) {
+                return new static($user);
+            }
+        }*/
+
+        return null;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+/*        foreach (self::$users as $user) {
+            if (strcasecmp($user['username'], $username) === 0) {
+                return new static($user);
+            }
+        }*/
+
+        return null;
+    }
+
+    public static function findByUsernameAndPassword($username, $password)
+    {
+        if (strpos($username, "@asp.messina.it") === false)
+            $username .= "@asp.messina.it";
+        $ldap = ldap_connect("asp.messina.it");
+        print_r($ldap);
+        if ($ldap) {
+            ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);  // Imposta la versione del protocollo LDAP
+            ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+
+            $binding = @ldap_bind($ldap, $username, $password);  // sostituisci con le credenziali dell'utente
+            if (!$binding) {
+                return null;
+            }
+            ldap_unbind($ldap); // disconnetti dal server LDAP
+        } else {
+            return null;
+        }
+
+        return new User([
+            "id" => $username,
+            "username" => str_replace("@asp.messina.it", "", $username),
+            "password" => $password
+        ]) ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+}
